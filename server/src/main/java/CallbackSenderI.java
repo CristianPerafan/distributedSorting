ab6f45633411fe888a6ee0d1cdcb74e7ecd1c86b
Cristian Perafan <1110282139@u.icesi.edu.co>
2023-11-30 23:08:41 -0500

    Feat: Add monolitic mergesort implementation

--- server/src/main/java/CallbackSenderI.java
+++ server/src/main/java/CallbackSenderI.java
@@ -1,11 +1,10 @@
 import Demo.CallbackReceiverPrx;
-import com.zeroc.Ice.Current;
-
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.security.PublicKey;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -18,6 +17,8 @@ public final class CallbackSenderI implements Demo.CallbackSender
 {
 
     static Map<String,CallbackReceiverPrx> clients = new HashMap<>();
+
+    static MergeSort<ComparableClass> mergeSort = new MergeSort<ComparableClass>();
 
     @Override
     public void sendMessage(CallbackReceiverPrx proxy, String msg,com.zeroc.Ice.Current current)
@@ -40,7 +41,7 @@ public final class CallbackSenderI implements Demo.CallbackSender
     }
 
     public String evaluateOrder(String msg,CallbackReceiverPrx proxy){
-        StringBuilder out = new StringBuilder("");
+        String out = "";
 
         String [] msgArray = msg.split("-");
         String order = msgArray[msgArray.length-1];
@@ -48,149 +49,61 @@ public final class CallbackSenderI implements Demo.CallbackSender
         if(order.startsWith("dist_sorter")){
             String [] orderArray = order.split(":");
             String file = orderArray[orderArray.length-1];
-            System.out.println(file);
+            initializeSorting(file);
         }
 
 
-        return out.toString();
+        return out;
     }
 
-    public static StringBuilder runCommandInConsole(String command) {
-        StringBuilder output = new StringBuilder();
-        try {
-            Process process = Runtime.getRuntime().exec(command);
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+    public static void initializeSorting(String filename){
 
+        long startTime = System.currentTimeMillis();
+
+        String filePath = "C:/Users/Cristian Perafan/Downloads/ejemplo.dat.txt";
+
+        List<ComparableClass> dataList = readFile(filePath);
+
+        dataList = mergeSort.mergeSort(dataList);
+
+        long endTime = System.currentTimeMillis();
+        long elapsedTime = endTime - startTime;
+
+        System.out.println("File reading time: " + elapsedTime + " milliseconds");
+
+    }
+
+    private static List<ComparableClass> readFile(String filePath){
+
+
+        List<ComparableClass> dataList = new ArrayList<>();
+
+        try{
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
-            while ((line = reader.readLine()) != null) {
-                output.append(line).append("\n");
+            while((line = reader.readLine()) != null){
+                dataList.add(new ComparableClass(line));
             }
-
-            process.waitFor();
-            reader.close();
-
-        } catch (IOException | InterruptedException e) {
+        }
+        catch (IOException e){
             e.printStackTrace();
         }
 
-        return output;
+        return dataList;
     }
-    public static boolean isPositiveInteger(int n) {
-        return n > 0;
-    }
-    public static int convertStringToInt(String input) {
-        int result = 0;
-
-        try {
-            result = Integer.parseInt(input);
-
-        }catch (NumberFormatException e){
-            e.printStackTrace();
-        }
 
 
-        return result;
-    }
+
+
 
     public static CompletableFuture<Void> sendPrimeFactor(int n, String hostname) {
         return CompletableFuture.runAsync(() -> {
-            StringBuilder primeFactors = primeFactorsMethod(n);
 
             CallbackReceiverPrx prx = clients.get(hostname);
-            prx.receiveMessage(primeFactors.toString());
+            prx.receiveMessage("");
         });
     }
 
-    public static StringBuilder primeFactorsMethod(int n) {
-        StringBuilder result = new StringBuilder();
-
-        result.append(n).append("|");
-
-        for (int i = 2; i <= n; i++) {
-            if (isPrime(i) == 1) {
-                int x = n;
-                while (x % i == 0) {
-                    result.append(i).append(" ");
-                    x /= i;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    public static int isPrime(int num) {
-        if (num < 2) {
-            return 0; // No es primo
-        }
-
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return 0; // No es primo
-            }
-        }
-
-        return 1; // Es primo
-    }
-    public Boolean validateClientIsRegistered(String hostname){
-        return clients.containsKey(hostname);
-    }
-
-    public String broadcastMessage(String order,String hostname){
-        String out = "";
-
-        String[] orderArray = order.split(" ");
-
-        for (CallbackReceiverPrx prx : clients.values()) {
-            prx.receiveMessage("(BROADCAST) from "+hostname+" :"+orderArray[orderArray.length-1]);
-        }
-
-        return out;
-    }
-
-    public  String sendMessage(String order,String hostname){
-        String out = "";
-
-        String[] orderArray = order.split(" ");
-
-
-        if(clients.containsKey(orderArray[1])){
-
-            CallbackReceiverPrx prx = clients.get(orderArray[1]);
-            prx.receiveMessage("from "+hostname+" :"+orderArray[orderArray.length-1]);
-            out = "[Message send to "+orderArray[1]+"]";
-
-        }
-        else{
-            out = "[Error sending message]";
-        }
-
-
-        return out;
-    }
-
-    public static boolean canConvertToInt(String str) {
-        try {
-            Integer.parseInt(str);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
-    public static String listClients() {
-        StringBuilder hostnameString = new StringBuilder();
-
-        for (String hostname : clients.keySet()) {
-            hostnameString.append(hostname).append(", ");
-        }
-
-        if (hostnameString.length() > 0) {
-            hostnameString.delete(hostnameString.length() - 2, hostnameString.length());
-        }
-
-        return hostnameString.toString();
-    }
 
     @Override
     public void shutdown(com.zeroc.Ice.Current current)
